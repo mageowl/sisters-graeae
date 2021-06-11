afbbb3be102f83c147b7716fd19eb5a1078b8197
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-06-11 12:29:38 -0700

    add phaser base

---
+++ src/classes/scenes/GameScene.js
@@ -0,0 +1,1 @@
+export default class GameScene extends Phaser.Scene {}
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import GameScene from "./classes/scenes/GameScene.js";
+
+const game = new Phaser.Game({
+	type: Phaser.AUTO,
+	scale: {
+		mode: Phaser.Scale.RESIZE
+	},
+	physics: {
+		default: "arcade"
+	},
+	render: {
+		pixelArt: true
+	},
+	scene: GameScene
+});
