73884ab7264ce532bf4d01034707b60d5d02736f
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-06-13 10:16:22 -0700

    yep

--- src/classes/scenes/UIScene.js
+++ src/classes/scenes/UIScene.js
@@ -1,21 +1,24 @@
 export default class UIScene extends Phaser.Scene {
-  sec = 295;
+	sec = 295;
 
-  constructor() {
-    super("ui");
-  }
+	constructor() {
+		super("ui");
+	}
 
-  create() {
-    this.timer = this.add.text(16, 16, "⏱ 300").setFontSize(20);
-    this.time.addEvent({
-      delay: 1000,
-      callback: this.updateTimer,
-      repeat: 300,
-    });
-  }
-  updateTimer = () => {
-    this.sec++;
-    if (this.sec === 300) this.scene.start("lose");
-    this.timer.setText(`⏱ ${300 - this.sec}`);
-  };
+	create() {
+		this.timer = this.add.text(16, 16, "⏱ 300").setFontSize(20);
+		this.time.addEvent({
+			delay: 1000,
+			callback: this.updateTimer,
+			repeat: 300
+		});
+	}
+	updateTimer = () => {
+		this.sec++;
+		if (this.sec === 300) {
+			this.scene.start("lose");
+			this.scene.stop("game");
+		}
+		this.timer.setText(`⏱ ${300 - this.sec}`);
+	};
 }
