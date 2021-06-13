e6473741a5b02b334331f2ad28719e7f2c767f64
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-06-12 18:04:48 -0700

    zeus cutscene

---
+++ src/classes/scenes/ZeusPrescript.js
@@ -0,0 +1,27 @@
+import createPrescriptedScene from "./PrescriptedScene.js";
+
+export default createPrescriptedScene("zeus", {
+	images: [
+		{ key: "zeus", url: "sprites/zeus.png" },
+		{ key: "sister-idle", url: "sprites/sister-idle.png" }
+	],
+	sprites: [
+		{ key: "zeus", x: 80, y: 100 },
+		{ key: "sister-idle", name: "sis0" }
+	],
+	bgImage: "sprites/title-bg.png",
+	actions: [
+		{
+			type: "wait",
+			delay: 500
+		},
+		{
+			type: "move",
+			sprite: "zeus",
+			time: 1000,
+			x: 80,
+			y: 80
+		}
+	],
+	next: "game"
+});
