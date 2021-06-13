43966329c4383aaaf418543c4fcb197d6736d3e5
Michael Leggett <leggett@gmail.com>
2021-06-12 18:24:25 -0700

    Update ZeusPrescript.js

--- src/classes/scenes/ZeusPrescript.js
+++ src/classes/scenes/ZeusPrescript.js
@@ -4,10 +4,13 @@ export default createPrescriptedScene("zeus", {
   images: [
     { key: "zeus", url: "sprites/zeus.png" },
     { key: "sister-idle", url: "sprites/sister-idle.png" },
+    { key: "sister-ne-idle", url: "sprites/sister-no-eye.png" },
   ],
   sprites: [
     { key: "zeus", x: 80, y: 100 },
-    { key: "sister-idle", name: "sis0" },
+    { key: "sister-ne-idle", name: "sis0", x: 70, y: 25 },
+    { key: "sister-idle", name: "sis1", x: 80, y: 25 },
+    { key: "sister-ne-idle", name: "sis2", x: 90, y: 25 },
   ],
   bgImage: "sprites/title-bg.png",
   actions: [
@@ -20,11 +23,16 @@ export default createPrescriptedScene("zeus", {
       sprite: "zeus",
       time: 1000,
       x: 80,
-      y: 80,
+      y: 70,
+    },
+    {
+      type: "dialog",
+      text: "Testing stuff",
+      speaker: "Zeus",
     },
     {
       type: "wait",
-      delay: 5000,
+      delay: 10000,
     },
   ],
   next: "game",
